47c63dc44c56e19d720cf5bffdcd578f232488d6
IngridCriss <ingridcrissenai@gmail.com>
2024-02-20 17:22:25 -0300

    Tela de contato feita e tabela iniciada

---
+++ Contato/script.js
@@ -0,0 +1,36 @@
+var nomeGlobal;
+var mensagemGlobal;
+var dateGlobal;
+
+function formatarData(date) {
+    var options = {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+        hour: "numeric",
+        minute: "numeric"
+    };
+    return date.toLocaleString("pt-BR", options);
+}
+
+function conferirMensagemWpp() {
+    var nome = document.getElementById("nome").value;
+    var mensagem = document.getElementById("mensagem").value;
+    var date = new Date();
+
+    nomeGlobal = nome;
+    mensagemGlobal = mensagem;
+    dateGlobal = date;
+
+    document.getElementById("confNome").textContent = nome;
+    document.getElementById("confMsg").textContent = mensagem;
+    document.getElementById("confDate").textContent = formatarData(date);
+}
+
+function enviarWpp() {
+    var numeroTelefone = "5541999999999";
+
+    var link = "https://wa.me/" + numeroTelefone + "?text= NOME DO FÃ DA BARBIE" + nomeGlobal + " - " + mensagemGlobal + " - " + formatarData(dateGlobal);
+    console.log(link);
+    window.open(link, "_blank");
+}
--- README.md
+++ README.md
@@ -6,6 +6,9 @@ cd ..    volta uma pasta
 cd \     vai para outra pasta
 ctrl d    abre árvore
 ctrl
+windows .     emoji
+UTF8          site emojis
 
 git clone - clona o projeto do rep remoto para o local
 
+<a href="../Contato/page.html">
